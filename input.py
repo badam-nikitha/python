35c9f7e42c56e8b379075b1f7a63042e2fa1b4be
Badam Nikitha <badam.nikitha@covalensedigital.net>
2022-05-27 12:38:57 +0530

    python input exercise

---
+++ input.py
@@ -0,0 +1,9 @@
+name = input("Enter Employee Name: ")
+salary = input("Enter salary: ")
+company = input("Enter Company name: ")
+
+
+print("\n")
+print("Printing Employee Details")
+print("Name", "Salary", "Company")
+print(name, salary, company)
